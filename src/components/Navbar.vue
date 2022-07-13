de7fad062b15ba58329a9106021ce8ea41e1cc01
anlianaaguilar <anlianaaguilar@gmail.com>
2022-07-12 21:57:26 -0400

    navbar funcional

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,21 +10,26 @@
       <v-list nav dense>
         <v-list-item-group
           v-model="group"
-          active-class="grey darken-1--text text--accent-4"
+          active-class="grey lighten-3--text text--accent-4"
         >
-          <v-list-item>
+          <v-list-item
+            v-for="route in routes"
+            :key="route.name"
+            @click="redirectTo(route.name)"
+            :disabled="currentRoute==route.name"
+          >
             <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
+              <v-icon>{{ route.icon }}</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
+            <v-list-item-title>{{ route.title }}</v-list-item-title>
           </v-list-item>
-
+          <!-- 
           <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-account</v-icon>
             </v-list-item-icon>
             <v-list-item-title>Account</v-list-item-title>
-          </v-list-item>
+          </v-list-item> -->
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
@@ -39,10 +44,32 @@ export default {
     return {
       drawer: false,
       group: null,
+      routes: [
+        // se crean para hacer redireccion programatica
+        {
+          icon: "mdi-home",
+          title: "Inicio",
+          name: "home",
+        },
+        {
+          icon: "mdi-table-large-plus",
+          title: "Tabla",
+          name: "table",
+        },
+      ],
     };
   },
-  // computed: {},
-  //methods: {}
+  computed: {
+      currentRoute(){
+        return this.$route.name
+      }
+  },
+  methods: {
+    redirectTo(nameRoute) {
+      //con el metodo se programa la redireccion
+      this.$router.push({name:nameRoute});
+    },
+  },
   // watch: {},
   // components: {},
   // mixins: [],
--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,7 @@ const routes = [
   },
   {
     path: '/table',
+    name:'table',
     component: TableView
   },
   {
