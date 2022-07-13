8007b491f8f44854ca85c17c9455368f27ea25d1
anlianaaguilar <anlianaaguilar@gmail.com>
2022-07-12 20:54:00 -0400

    se crea tableview

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TableView from '@/views/TableView.vue'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,10 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path: '/table',
+    component: TableView
   },
   {
     path: '/about',
