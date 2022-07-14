b0bd4d5133e414eeb41b74b42dd688ba8868d505
anlianaaguilar <anlianaaguilar@gmail.com>
2022-07-14 18:24:32 -0400

    se modifica characterSearch

--- src/components/CharacterSearch.vue
+++ src/components/CharacterSearch.vue
@@ -14,35 +14,47 @@
         <v-btn elevation="2" @click="searchPokemon">Buscar</v-btn>
       </v-col>
     </v-row>
-    <v-row>
-      <v-col
-        cols="12"
-        sm="6"
-        md="4"
+    <v-row class="text-capitalize">
+      <v-col cols="12" sm="6" md="4"
         v-for="pokemonCharacter in pokemons"
         :key="pokemonCharacter.id"
       >
         <v-card class="mx-auto" max-width="344">
           <v-img :src="pokemon.imagen" height="200px"></v-img>
-          <v-card-title>{{ pokemon.name }}</v-card-title>
-
+          <v-card-title>Pokémon: {{ pokemon.name }}</v-card-title>
           <!-- modal -->
           <div class="text-center">
             <v-dialog v-model="dialog" width="50%">
               <template v-slot:activator="{ on, attrs }">
-                <v-btn color="grey darken-3  text-uppercase mb-3" dark v-bind="attrs" v-on="on">
+                <v-btn
+                  color="grey darken-3  text-uppercase mb-3"
+                  dark
+                  v-bind="attrs"
+                  v-on="on"
+                >
                   Ver mas
                 </v-btn>
               </template>
-
-              <v-card>
+              <v-card class="text-capitalize">
                 <v-card-title class="text-h5 grey lighten-2">
-                  {{ pokemon.name }}
+                  Pokémon: {{ pokemon.name }}
                 </v-card-title>
-
-                <v-card-text>
-                  <v-img :src="pokemon.front_shiny" height="200px"></v-img>
-                  <v-img :src="pokemon.back_shiny" height="200px"></v-img>
+                <v-card-text class="text-h5">
+                  <p class="text-decoration-underline">Tipo:</p>
+                  <div v-for="(typeP, index) in theTypes" :key="index">
+                    - {{ typeP }}
+                  </div>
+                  <p class="mb-0 mt-2 text-decoration-underline">
+                    Habilidades:
+                  </p>
+                  <div v-for="itemAbility in theAbility" :key="itemAbility.id">
+                    - {{ itemAbility }}
+                  </div>
+                  <div class="mt-2">
+                    <p class="text-decoration-underline">Version:</p>
+                    - Shiny
+                    <v-img :src="pokemon.front_shiny" height="200px"></v-img>
+                  </div>
                 </v-card-text>
 
                 <v-divider></v-divider>
@@ -75,14 +87,28 @@ export default {
         //se crea objeto de pokemon encontrado
         name: "",
         imagen: "",
+        type: "",
+        ability: "",
         back_default: "",
         back_shiny: "",
         front_shiny: "",
       },
       pokemons: [], // se guardan todos los aldeanos buscados
+      dialog: false,//para el modal
     };
   },
-  // computed: {},
+  computed: {
+    theTypes() {
+      return this.pokemon.type.map(function (elemt) {
+        return elemt.type.name;
+      });
+    },
+    theAbility() {
+      return this.pokemon.ability.map(function (elemt) {
+        return elemt.ability.name;
+      });
+    },
+  },
   methods: {
     async searchPokemon() {
       try {
@@ -100,7 +126,8 @@ export default {
     setPokemon(pokemonData) {
       this.pokemon.name = pokemonData.name;
       this.pokemon.imagen = pokemonData.sprites.front_default;
-      this.pokemon.back_shiny = pokemonData.sprites.back_shiny;
+      this.pokemon.type = pokemonData.types;
+      this.pokemon.ability = pokemonData.abilities;
       this.pokemon.front_shiny = pokemonData.sprites.front_shiny;
       console.log(this.pokemon);
 
